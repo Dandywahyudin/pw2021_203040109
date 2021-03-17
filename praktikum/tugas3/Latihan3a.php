73a557be50a179fc8aa64f067f44867950caddd4
Dandywahyudin <75318832+Dandywahyudin@users.noreply.github.com>
2021-03-17 23:47:41 +0700

    latihan3

---
+++ praktikum/tugas3/Latihan3a.php
@@ -0,0 +1,10 @@
+<?php 
+
+$array = ["ada","abel","men","pung","nilai"];
+    echo "Array ".$array[0]."lah suatu vari".
+                  $array[1]." yang dapat ".
+                  $array[2]."am".
+                  $array[3]." banyak ".
+                  $array[4];
+?>
+
